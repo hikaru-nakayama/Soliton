407648f07630c318c4baa0ed250393d009743893
hikaru-nakayama <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>
2023-09-17 19:27:18 +0900

    :bulb: Add TODO comments

--- lib/soliton/main.rb
+++ lib/soliton/main.rb
@@ -7,6 +7,7 @@ require "soliton/test"
 module Soliton
   class App
     def self.start
+      # ここに middleware を追加する処理を書く
       app = Application.new(Router.instance)
       server = Server.new(app)
       server.start
